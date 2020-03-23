1212a6db5dc241be4238917fcf90afffdf7559ff
zhaozhe0404 <47239364+zhaozhe0404@users.noreply.github.com>
2020-03-23 12:04:23 +0800

    Update if_else.sql

--- control/if_else.sql
+++ control/if_else.sql
@@ -1,1 +1,12 @@
 
+declare
+  v_num1 number := 0;
+  v_num2 number;
+begin
+  if v_num1 = v_num2    
+  then
+    dbms_output.put_line('v_num1 == v_num2');
+  else
+    dbms_output.put_line('v_num1 == v_num2');    
+  end if;
+end;
